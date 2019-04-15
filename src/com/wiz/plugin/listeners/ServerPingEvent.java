117e5d4ac7c9c758bc02183ddfffe9e41e7bb7f0
Wiz_Techno <wiz.techno@nomail.fff>
2019-04-14 20:15:46 -0400

    Code for Pombo

---
+++ src/com/wiz/plugin/listeners/ServerPingEvent.java
@@ -0,0 +1,13 @@
+package com.wiz.plugin.listeners;
+
+import net.md_5.bungee.api.plugin.Listener;
+
+public class ServerPingEvent implements Listener {
+    public void onServerPing(ServerPingEvent e)
+    {
+
+
+    }
+
+
+}
